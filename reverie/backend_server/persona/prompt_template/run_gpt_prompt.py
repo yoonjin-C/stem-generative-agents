74576b8de237967910d8e3be0353be1fe6b1d3b8
Yoonjin Choi <cyy000123@gmail.com>
2025-04-14 00:50:44 +0900

    🐛 Fix func_clean_up def

--- reverie/backend_server/persona/prompt_template/run_gpt_prompt.py
+++ reverie/backend_server/persona/prompt_template/run_gpt_prompt.py
@@ -356,56 +356,84 @@ def run_bedrock_prompt_task_decomp(persona,
     print ("-==- -==- -==- ")
 
     # TODO SOMETHING HERE sometimes fails... See screenshot
-    temp = [i.strip() for i in bedrock_response.split("\n")]
-    _cr = []
-    cr = []
-    for count, i in enumerate(temp): 
-      if count != 0: 
-        _cr += [" ".join([j.strip () for j in i.split(" ")][3:])]
-      else: 
-        _cr += [i]
-    for count, i in enumerate(_cr): 
-      k = [j.strip() for j in i.split("(duration in minutes:")]
-      task = k[0]
-      if task[-1] == ".": 
-        task = task[:-1]
-      duration = int(k[1].split(",")[0].strip())
-      cr += [[task, duration]]
+    # temp = [i.strip() for i in bedrock_response.split("\n")]
+    # _cr = []
+    # cr = []
+    # for count, i in enumerate(temp): 
+    #   if count != 0: 
+    #     _cr += [" ".join([j.strip () for j in i.split(" ")][3:])]
+    #   else: 
+    #     _cr += [i]
+    # for count, i in enumerate(_cr): 
+    #   k = [j.strip() for j in i.split("(duration in minutes:")]
+    #   task = k[0]
+    #   if task[-1] == ".": 
+    #     task = task[:-1]
+    #   duration = int(k[1].split(",")[0].strip())
+    #   cr += [[task, duration]]
 
-    total_expected_min = int(prompt.split("(total duration in minutes")[-1]
-                                   .split("):")[0].strip())
+    try:
+        # 응답을 줄 단위로 분리
+        lines = [line.strip() for line in bedrock_response.split('\n') if line.strip()]
+        cr = []
+
+        for line in lines:
+            if not line.startswith(('1)', '2)', '3)', '4)', '5)', '6)', '7)', '8)', '9)')):
+                continue
+
+            # 괄호 안의 시간 정보 추출
+            try:
+                task = line.split(')')[1].split('(')[0].strip()
+                duration_str = line.split('(')[1].split(',')[0].strip()
+                duration = int(''.join(filter(str.isdigit, duration_str)))
+                
+                if task[-1] == '.':
+                    task = task[:-1]
+                    
+                cr.append([task, duration])
+            except:
+                continue
+
+        if not cr:
+            return get_fail_safe()
+
+        total_expected_min = int(prompt.split("(total duration in minutes")[-1]
+                                      .split("):")[0].strip())
     
-    # TODO -- now, you need to make sure that this is the same as the sum of 
-    #         the current action sequence. 
-    curr_min_slot = [["dummy", -1],] # (task_name, task_index)
-    for count, i in enumerate(cr): 
-      i_task = i[0] 
-      i_duration = i[1]
+        # TODO -- now, you need to make sure that this is the same as the sum of 
+        #         the current action sequence. 
+        curr_min_slot = [["dummy", -1],] # (task_name, task_index)
+        for count, i in enumerate(cr): 
+          i_task = i[0] 
+          i_duration = i[1]
 
-      i_duration -= (i_duration % 5)
-      if i_duration > 0: 
-        for j in range(i_duration): 
-          curr_min_slot += [(i_task, count)]       
-    curr_min_slot = curr_min_slot[1:]   
+          i_duration -= (i_duration % 5)
+          if i_duration > 0: 
+            for j in range(i_duration): 
+              curr_min_slot += [(i_task, count)]       
+        curr_min_slot = curr_min_slot[1:]   
 
-    if len(curr_min_slot) > total_expected_min: 
-      last_task = curr_min_slot[60]
-      for i in range(1, 6): 
-        curr_min_slot[-1 * i] = last_task
-    elif len(curr_min_slot) < total_expected_min: 
-      last_task = curr_min_slot[-1]
-      for i in range(total_expected_min - len(curr_min_slot)):
-        curr_min_slot += [last_task]
+        if len(curr_min_slot) > total_expected_min: 
+          last_task = curr_min_slot[60]
+          for i in range(1, 6): 
+            curr_min_slot[-1 * i] = last_task
+        elif len(curr_min_slot) < total_expected_min: 
+          last_task = curr_min_slot[-1]
+          for i in range(total_expected_min - len(curr_min_slot)):
+            curr_min_slot += [last_task]
 
-    cr_ret = [["dummy", -1],]
-    for task, task_index in curr_min_slot: 
-      if task != cr_ret[-1][0]: 
-        cr_ret += [[task, 1]]
-      else: 
-        cr_ret[-1][1] += 1
-    cr = cr_ret[1:]
+        cr_ret = [["dummy", -1],]
+        for task, task_index in curr_min_slot: 
+          if task != cr_ret[-1][0]: 
+            cr_ret += [[task, 1]]
+          else: 
+            cr_ret[-1][1] += 1
+        cr = cr_ret[1:]
 
-    return cr
+        return cr
+    except Exception as e:
+        print(f"Clean up error: {e}")
+        return get_fail_safe()
 
   def __func_validate(bedrock_response, prompt=""): 
     # TODO -- this sometimes generates error 
